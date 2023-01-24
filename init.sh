a09b3c36e84400324948c6781e0badcc01a24274
pedevineau <pedevineau@hotmail.fr>
2023-01-24 17:09:08 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,16 @@
-python3.10 -m pip install -r requirements.txt
+#!/bin/sh
+
+WORK_DIR=/home/onyxia/work
+CLONE_DIR=${WORK_DIR}/load-test
+
+# Clone course repository
+REPO_URL=git@github.com:pedevineau/load-siret.git
+git clone --depth 1 $REPO_URL $CLONE_DIR
+
+# Install additional packages if needed
+REQUIREMENTS_FILE=${CLONE_DIR}/requirements.txt
+[ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE && rm $REQUIREMENTS_FILE
+
+cd $CLONE_DIR
+
+chown onyxia -R /home/onyxia/ # Sinon onyxia n'a plus les droits
